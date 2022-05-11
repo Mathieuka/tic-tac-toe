95dcfaff1faac4f36b2b99ff6b4d2e3fc56a8ba0
mk <mathieu.kahlaoui.pro@gmail.com>
2022-05-11 15:08:21 +0200

    feat: handle player's turn

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,32 @@ import styles from "./App.module.css";
 
 function App() {
   const [squares, setSquares] = useState<string[]>(Array.from({ length: 9 }));
+  const [player, setPlayer] = useState("1");
 
   const onCheckSquare = (idx: number) => {
-    const newSquares = [...squares];
-    if (!newSquares[idx]) {
+    if (!squares[idx]) {
+      const newSquares = [...squares];
       newSquares[idx] = "X";
       setSquares(newSquares);
+      setPlayer(() => (player === "1" ? "2" : "1"));
     }
   };
 
   return (
-    <div className={styles.app}>
-      <div className={styles.container}>
-        {squares.map((value: string, index) => (
-          <div
-            key={Math.random() * index}
-            className={styles.square}
-            onClick={() => onCheckSquare(index)}
-          >
-            {value}
-          </div>
-        ))}
+    <div>
+      <div>Player's {player} turn to play</div>
+      <div className={styles.app}>
+        <div className={styles.container}>
+          {squares.map((value: string, index) => (
+            <div
+              key={Math.random() * index}
+              className={styles.square}
+              onClick={() => onCheckSquare(index)}
+            >
+              {value}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
