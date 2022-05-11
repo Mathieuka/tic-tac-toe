c764809ed4638f950ef4caff00b6750535010e17
mk <mathieu.kahlaoui.pro@gmail.com>
2022-05-11 14:17:51 +0200

    feat: create responsive UI layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,17 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.app}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.square}>0</div>
+        <div className={styles.square}>1</div>
+        <div className={styles.square}>2</div>
+        <div className={styles.square}>3</div>
+        <div className={styles.square}>4</div>
+        <div className={styles.square}>5</div>
+        <div className={styles.square}>6</div>
+        <div className={styles.square}>7</div>
+        <div className={styles.square}>8</div>
+      </div>
     </div>
   );
 }
